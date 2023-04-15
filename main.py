3c74d6543afa531d80b9e7c727aba8afd08a3aa1
Stanislaw Dutkiewicz <stan.dutkiewicz@hotmail.com>
2023-04-15 12:13:45 +0200

    modyfying delet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, abort
 from builtins import enumerate
 
 app = Flask(__name__)
@@ -26,6 +26,28 @@ def add_task():
     if task:
         with open('tasks.txt', 'a') as file:
             file.write(task + '\n')
+
+    # Redirect back to the home page
+    return redirect('/')
+
+@app.route('/delete_task/<int:id>', methods=['POST'])
+def delete_task(id):
+    # Read the contents of the tasks.txt file
+    with open('tasks.txt', 'r') as file:
+        tasks = file.readlines()
+
+    # Find the task with the specified ID and remove it from the list
+    task_found = False
+    with open('tasks.txt', 'w') as file:
+        for task in tasks:
+            if not task.startswith(f'{id}\t'):
+                file.write(task)
+            else:
+                task_found = True
+
+    # If the task is not found, abort with a 404 error
+    if not task_found:
+        abort(404)
 
     # Redirect back to the home page
     return redirect('/')
